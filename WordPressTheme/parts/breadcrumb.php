eb94d9017fae1e41735ad50be191b8de9d9f338a
Naoko705310 <naokos79@gmail.com>
2025-09-30 13:23:26 +0900

    エントリーの上部がheaderに重ならないように修正

--- WordPressTheme/parts/breadcrumb.php
+++ WordPressTheme/parts/breadcrumb.php
@@ -1,18 +1,8 @@
 <!-- パンくずリスト -->
-<div class=" breadcrumb">
-    <div class="breadcrumb__inner inner">
-    <?php if(function_exists('bcn_display'))
-    {
-      bcn_display();
-    }?>
-    </div>
-  </div>
-
-
-
-
-<!-- <div class="breadcrumb">
+<div class="breadcrumb <?php echo is_page('entry') ? 'page-entry__breadcrumb' : ''; ?>">
   <div class="breadcrumb__inner inner">
-    <?php my_breadcrumb(); ?>
+    <?php if (function_exists('bcn_display')) {
+      bcn_display();
+    } ?>
   </div>
-</div> -->
+</div>
